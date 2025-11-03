37c404dc9761f18edf09aa4b26dd3c93f0a412fd
BOBSKYTH <sroytaninwongsatum27@gmail.com>
2025-11-03 20:51:36 +0700

    aaa

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -29,10 +29,6 @@ public abstract class Weapon : MonoBehaviour
 
 
 
-
-
-
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
